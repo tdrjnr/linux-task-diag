01bc7db9d60cc9d331adbf37ffbffab09bd5506a
Andrey Vagin <avagin@openvz.org>
2014-03-04 19:29:18 +0400

    tcp: allow to enable repair mode for sockets in any state
    The repair mode is used for dumping state of tcp connections
    (sequence numbers, queues, options, etc).
    
    Currently the repair mode can be enalbed only for sockets in the
    TCP_ESTABLISHED state. If a socket in another state, its internal
    state can not be dumped.
    
    Same time there is no guarantee that a connection won't be in other
    states when we are dumping it, thus to be able to dump and restore
    such states we need to get rid of CLOSE,ESTABLISHED in-kernel
    limitation.
    
    I see nothing wrong to allow enabling of the repair mode for connected
    sockets in any states.
    
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Alexey Kuznetsov <kuznet@ms2.inr.ac.ru>
    Cc: James Morris <jmorris@namei.org>
    Cc: Hideaki YOSHIFUJI <yoshfuji@linux-ipv6.org>
    Cc: Patrick McHardy <kaber@trash.net>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Pavel Emelyanov <xemul@parallels.com>
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -1132,14 +1132,17 @@ int tcp_sendmsg(struct sock *sk, struct msghdr *msg, size_t size)
 	}
 
 	if (unlikely(tp->repair)) {
+		err = -EINVAL;
+		if (tp->repair_queue == TCP_NO_QUEUE)
+			goto out_err;
+
+		if (sk->sk_state != TCP_ESTABLISHED)
+			goto out_err;
+
 		if (tp->repair_queue == TCP_RECV_QUEUE) {
 			copied = tcp_send_rcvq(sk, msg, size);
 			goto out_nopush;
 		}
-
-		err = -EINVAL;
-		if (tp->repair_queue == TCP_NO_QUEUE)
-			goto out_err;
 
 		/* 'common' sending to sendq */
 	}
@@ -2302,7 +2305,7 @@ EXPORT_SYMBOL(tcp_disconnect);
 static inline bool tcp_can_repair_sock(const struct sock *sk)
 {
 	return ns_capable(sock_net(sk)->user_ns, CAP_NET_ADMIN) &&
-		((1 << sk->sk_state) & (TCPF_CLOSE | TCPF_ESTABLISHED));
+		(sk->sk_state != TCP_LISTEN);
 }
 
 static int tcp_repair_set_window(struct tcp_sock *tp, char __user *optbuf, int len)
