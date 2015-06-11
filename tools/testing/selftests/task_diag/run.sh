2a60ebac5af4d6061bcb52c84abf609f1026eeda
David Ahern <dsahern@gmail.com>
2015-06-11 14:01:27 -0600

    task_diag: Enhance fork tool to spawn threads
    Add option to fork threads as well as processes.
    Make the sleep time configurable too so that spawned
    tasks exit on their own.
    
    Signed-off-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: Andrey Vagin <avagin@openvz.org>

--- tools/testing/selftests/task_diag/run.sh
+++ tools/testing/selftests/task_diag/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-./fork 1000
+./fork 1000 10
 nproc=`./task_diag_all A | grep 'pid.*tgid.*ppid.*comm fork$' | wc -l`
 killall -9 fork
-[ "$nproc" -eq 1000 ] && exit 0
+[ "$nproc" -eq 10000 ] && exit 0
 echo "Unexpected number of tasks '$nproc'" 1>&2
