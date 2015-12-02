94c198200da845dadf26ac67effd21567e21e9d1
Andrey Vagin <avagin@openvz.org>
2015-12-02 17:59:21 +0300

    test: check that task_diag can dump all thread of one process

--- tools/testing/selftests/task_diag/_run.sh
+++ tools/testing/selftests/task_diag/_run.sh
@@ -10,11 +10,15 @@ nchildren=`./task_diag_all children --pid 1 | grep 'pid.*tgid.*ppid.*comm fork$'
 
 ./task_diag_all one --pid 1 --pidns 1 --cred
 
+( exec -a fork_thread ./fork 1 1234 )
+pid=`pidof fork_thread`
+ntaskthreads=`./task_diag_all thread --maps --cred --smaps --pid $pid |  grep 'pid.*tgid.*ppid.*comm' | wc -l`
 killall -9 fork
 
 [ "$nthreads"     -eq 10000 ] &&
 [ "$nprocesses"   -eq 1000  ] &&
 [ "$nchildren"    -eq 1000  ] &&
+[ "$ntaskthreads" -eq 1234  ] &&
 true ||  {
 	echo "Unexpected number of tasks $nthreads:$nprocesses" 1>&2
 	exit 1
