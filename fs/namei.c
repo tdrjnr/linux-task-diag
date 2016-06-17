d3239acbcc978ca460642c37de069b1c5253f88f
Andrey Vagin <avagin@openvz.org>
2016-06-17 09:05:41 -0700

    namei: add LOOKUP_DFD_ROOT to use dfd as root
    The problem is that a pathname can contain absolute symlinks and now
    they are resolved relative to the current root.
    
    If we want to open a file in another mount namespaces and we have a file
    descriptor to its root directory, we probably want to resolve pathname
    in the target mount namespace. For this we add this new flag.
    
    If LOOKUP_DFD_ROOT is set, path_init() initializes nd->root and nd->path
    to the same value.
    
    Changes since v1:
    * initialize nd->root_seq (thanks to Omar Sandoval for reporting and
      fixing this issue)
    
    Cc: Omar Sandoval <osandov@osandov.com>

--- fs/namei.c
+++ fs/namei.c
@@ -2172,7 +2172,7 @@ static const char *path_init(struct nameidata *nd, unsigned flags)
 	nd->path.dentry = NULL;
 
 	nd->m_seq = read_seqbegin(&mount_lock);
-	if (*s == '/') {
+	if (*s == '/' && !(flags & LOOKUP_DFD_ROOT)) {
 		if (flags & LOOKUP_RCU)
 			rcu_read_lock();
 		set_root(nd);
@@ -2197,6 +2197,13 @@ static const char *path_init(struct nameidata *nd, unsigned flags)
 		} else {
 			get_fs_pwd(current->fs, &nd->path);
 			nd->inode = nd->path.dentry->d_inode;
+		}
+		if (flags & LOOKUP_DFD_ROOT) {
+			nd->root = nd->path;
+			if (flags & LOOKUP_RCU)
+				nd->root_seq = nd->seq;
+			else
+				path_get(&nd->root);
 		}
 		return s;
 	} else {
@@ -2226,6 +2233,13 @@ static const char *path_init(struct nameidata *nd, unsigned flags)
 			nd->inode = nd->path.dentry->d_inode;
 		}
 		fdput(f);
+		if (flags & LOOKUP_DFD_ROOT) {
+			nd->root = nd->path;
+			if (flags & LOOKUP_RCU)
+				nd->root_seq = nd->seq;
+			else
+				path_get(&nd->root);
+		}
 		return s;
 	}
 }
